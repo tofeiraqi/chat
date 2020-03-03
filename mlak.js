5dec7e8ffd30c8bcdf6563ecb279184981afddc3
tofeiraqi <44017333+tofeiraqi@users.noreply.github.com>
2020-03-03 11:28:12 -0800

    Update mlak.js

--- mlak.js
+++ mlak.js
@@ -16,4 +16,56 @@ $(`<link rel="stylesheet" href="https://www.iraqiaa.net/iraqiaa3.css
 													function saveColor(){
 														var dfsdfsdf = $('.label-primary, .btn-primary').css('background-color');
 														console.log(dfsdfsdf)
-														var colorLo = {bgcolor:$('.bgcolor').val(),btcolor:$('.btcolor').va
+														var colorLo = {bgcolor:$('.bgcolor').val(),btcolor:$('.btcolor').val(),bocolor:$('.bocolor').val(),hicolor:$('.hicolor').val()}
+														localStorage.setItem('colorLo', JSON.stringify(colorLo));
+														getLoColor(JSON.stringify(colorLo))
+														$('.divColorLo').slideToggle();
+													}
+													var hexDigits = new Array
+													("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+													function rgb2hex(rgb) {
+														rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+														return  hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+													}
+													function hex(x) {
+														return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+													}
+													function getLoColor(c){
+														var lo = localStorage.getItem('colorLo')||c;
+														if(lo){
+															var stLoc = localStorage.getItem('colorSt');
+															var loJs = JSON.parse(lo)
+															for(var i in loJs){
+																$('.'+i).val(loJs[i]);
+																switch(i){
+																	case 'bgcolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.bg').css('background-color'))
+																		break;
+																	case 'btcolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.label-primary, .btn-primary').css('background-color'))
+																		break;
+																	case 'bocolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.border').css('border-color'))
+																		break;
+																	case 'hicolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.light').css('background-color'))
+																		break;
+																}
+															}
+															var aa = '<style class="colorLo">.border{border-color: #'+loJs.bocolor+'!important;} .primaryborder{border-color: #'+loJs.btcolor+'!important;} .label-primary, .btn-primary {background-color: #'+loJs.btcolor+'!important;} .light{background-color: #'+loJs.hicolor+'!important;} .bg{background-color: #'+loJs.bgcolor+';}</style>';
+															if(stLoc && !c)aa = stLoc;
+															localStorage.setItem('colorSt', aa);
+															var loHtml = $('.colorLo')
+															if(loHtml.length >0)loHtml.text($(aa).text());
+															else $('head').append(aa);
+														}else{
+															$('.colorLo').remove();
+															$('.bgcolor,.btcolor,.hicolor,.bocolor').val('000000');
+															$('.bgcolor,.btcolor,.hicolor,.bocolor').css('background-color','#000000');
+														}
+													}
+													getLoColor()
+													$('.ssss').click(function(e){
+														e.stopPropagation();
+														$('.divColorLo').slideToggle();
+													})
