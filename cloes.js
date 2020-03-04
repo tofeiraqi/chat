8bd322bca7ea0f63be974935648bd1f17e7a9bff
tofeiraqi <44017333+tofeiraqi@users.noreply.github.com>
2020-03-04 07:21:18 -0800

    Update cloes.js

--- cloes.js
+++ cloes.js
@@ -1,4 +1,4 @@
-$('div#l1 button,div#l2 button,div#l3 button').attr('onclick', '');
+/*$('div#l1 button,div#l2 button,div#l3 button').attr('onclick', '');
 $('div#l1 button,div#l2 button,div#l3 button').click(function(){
 
 alert('الموقع مغلق :دوك موبايل');
