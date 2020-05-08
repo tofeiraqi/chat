fb8d327399d5bacf9502fdcb379709ab2f961075
tofeiraqi <44017333+tofeiraqi@users.noreply.github.com>
2020-05-08 15:23:36 -0700

    Update lmh0.js

--- lmh0.js
+++ lmh0.js
@@ -1,3 +1,46 @@
+$(`<img src="https://lmh0.com/sico/1588968063160.jpeg" class="fr" style="width:100%;margin-top: 0px;">
+`).insertBefore('.nav-tabs');
+$(`
+  <a id="des3" href="https://lmh0.com/" style="border: 1px solid white;transform:matrix(1, 0, 0, 1, 112, 112);border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-paint-brush btn btn-primary"></a>
+   <a id="des3" href="https://lmh0.com/" style="transform:matrix(1, 0, 0, 1, 74, 112);border: 1px solid white;border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-star btn btn-primary"></a>
+ <a id="des3" href="rules.html" style="transform:matrix(1, 0, 0, 1, 36, 43);border: 1px solid white;border-radius: 0px!important;width:10%;margin:-69px 2px 2px;" class="fl mini fa fa-phone btn btn-primary"></a>
+ <a id="des3" href="https://lmh0.com/" style="transform:matrix(1, 0, 0, 1, -2, 8);border: 1px solid white;border-radius: 15px 0px 0px 0px!important;width:10%;margin:-34px 2px 2px;" class="fl mini  fa   fa fa-heartbeat btn btn-primary"></a>
+<a id="des22" style="padding: 2px 0px!important;font-size: 12px !important;border: 1px solid white;border-radius: 0px 0px 0px 0px!important;width:100px;float:right;margin:-68px 2px 2px;transform:matrix(1, 0, 0, 1, 4, 24);" class="label label label-primary " target="_blank" href="https://lmh0.com/">لمة فلسطين</a>
+ `).insertBefore('.nav-tabs');
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://lmh0.com/sico/1588748514973.gif);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="    >
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">   -ابو عوقاب   </span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋‹ <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1>  نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات لمة فلسطين</div>
+      </div>
+    </div></div>
+    `).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+ $("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+ $.getScript("https://chatpalestine.me/chatjawwal33.js");
+$(`<center><div><div width="75.5%" style="background-color: #b7c3c5; color: #BF4A47;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"  
+><font style="background-color: #fff;border-radius: 0px 25px 0px 25px;font-family: 'Cairo', sans-serif, FontAwesome;padding: 2px 10px 2px 10px; margin:0px 25px 0px 25px;" >اهلا وسهلا بكم في لمة فلسطين</font></div></div><center>`).insertBefore('#d2');
+$('.break.light').css({"background-image":"url(https://lmh0.com/sico/1588970750710.jpg)"});
+(document.getElementById("tbox").placeholder = " اكتب رسالتك مع لمة فلسطين ");
+$(`<button onfa-microphone="alert('تجريبي')" style="width:31px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-video-camera fl btn btn-danger"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+$('.nav-tabs').addClass('fl').css('width','100%'),
+$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+
 eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$("2#f g.4.4-5").h(3(){d b=i(3(){d c=$(".A").j;0<c?($(`</2><7>`).B("#8"),C(b)):D.E(c)},1)}),$("2#f g.4.4-5").h(3(){k(3(){F()},0)}),i(3(){d a=$("#e");0<a.j&&a.G()},0),$(".e").l(`<2 H="8"m="n-I: 7;J-K: L M,N!O;P: #Q; R: o%;q:o%;"r="S T s-U V"></t><p></p>اهلا وسهلا بكم</p></p> ستكون المسابقه الكبرى / من سيربح المليون ،، يوم الخميس</p/>فى تمام الساعه ١١ مساءا</p>ويحصل الفائر على مليون لايك  </p>وهدية عباره عن</p>سوبر تاج لمدة شهر~ </p> روم بإسم الفائز لمدة شهر / فى انتظار الفائز / مع صاحب الموقع  </p> كما ستقام مسابقه الابداع الاسبوعيه يوم الاربعاء </p> في تمام الساعه الحادي عشر مساءاً</p>ًمدتها نصف ساعه ويحصل الفائزين</p>على سوبر تاج الابداع لمده اسبوع و لايكات من الاداره / مع صاحب الموقع</p>طابت جميع اوقاتكم بالخير والمسرات</p><a u="W://X.Y.v/"><Z 10="11://6.12.v/13.14"m="q:15%;16-17: 18;"></t><7></2><7></2>`),$(\'#19\').l(`<9 w="شات لمة فلسطين للجول"u="#"1a="$(\'.1b\').n($(x).1c(\'w\'));1d($(x),\'5\');k(3(){$(\'#8\').1e(0);},0);$(\'.e\').1f();"y-1g="s"y-1h="#8"r="1i z 9 9-5 z-1j">&1k;</9>`);',62,83,'||div|function|btn|primary||center|mic|label||||var|dpnl|tlogins|button|click|setInterval|length|setTimeout|append|style|text|100||width|class|tab|BR|href|net|title|this|data|fa|pmsgc|appendTo|clearInterval|console|log|refreshonline|remove|id|align|font|family|el|messiri|FontAwesome|important|color|072ADB|height|break|light|pane|border|http|www|1des|img|src|https|top4top|p_13950lw4e7|png|25|margin|top|0px|d0|onclick|pnhead|attr|hl|scrollTop|show|toggle|target|ae|star|nbsp'.split('|'),0,{}))
 $(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #B7B6BA;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;"> 
 (*لمعرفة تفاصيل المسابقات اضغط ايقونة النجمه )
