dd971e9e9276674012f4ef4bedf2b3a1ef563784
tofeiraqi <44017333+tofeiraqi@users.noreply.github.com>
2021-09-18 20:26:33 +0300

    Create nnew.js

---
+++ nnew.js
@@ -0,0 +1,62 @@
+$.getScript('https://oa20660.000webhostapp.com/chatnsh/iraqna/iraqna/user.js');
+$(`
+<div class="hand nosel fl uzr borderg uid174a8496ca9xdf880cb6-x-dsqcih inroom" style="text-align:left;background-color:white;border-radius:0px!important;width:99%;margin:0px;padding:1px;" v="10000">
+      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(&quot;pic/1600583176211.jpg&quot;);" class="fitimg fl u-pic ">
+      <label class="fl muted fa" style="color:indianred;">&nbsp;</label>
+      <img class="fr co" style="width:16px;border-radius:1px;" src="https://k.top4top.io/p_1724pk43t1.gif">
+      <span class="fr uhash" style="color:grey;font-size:70%!important;margin-top: -3px;">#1</span>
+      <div style="width:68%;" class="fl">
+        <div style="width:100%;margin-top:-2px;display: flex;" class="fl">
+          <img class="fl u-ico" style="object-fit: contain;min-height: 14px;" src="https://www.sudfachat.com/sico/1580233259851.gif">
+          <div class="fl" style="width:82%;"><span style="margin-top: 1px; padding: 0px 8px; max-width: 100%; border-radius: 3px; background-color: rgb(255, 255, 255); color: rgb(120, 120, 120);" class="corner u-topic dots">قوأت الردع السعوديه</span></div>
+        </div>
+        <div style="width:100%;color:#888;margin-top:-8px;" class="fl mini u-msg">دأم عــزك يـا وطــن</div>
+      </div>
+    </div>
+`).insertBefore(".nosel.ninr.fl.uzr.label.label-primary");
+$(`
+<div class="hand nosel fl uzr borderg uid174a8496ca9xdf880cb6-x-dsqcih inroom" style="text-align:left;background-color:white;border-radius:0px!important;width:80%;margin:0px;padding:1px;" v="10000">
+      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(&quot;pic/1600583176211.jpg&quot;);" class="fitimg fl u-pic ">
+      <label class="fl muted fa" style="color:indianred;">&nbsp;</label>
+      <img class="fr co" style="width:16px;border-radius:1px;" src="https://k.top4top.io/p_1724pk43t1.gif">
+      <span class="fr uhash" style="color:grey;font-size:70%!important;margin-top: -3px;">#1</span>
+      <div style="width:68%;" class="fl">
+        <div style="width:100%;margin-top:-2px;display: flex;" class="fl">
+          <img class="fl u-ico" style="object-fit: contain;min-height: 14px;" src="https://www.sudfachat.com/sico/1580233259851.gif">
+          <div class="fl" style="width:82%;"><span style="margin-top: 1px; padding: 0px 8px; max-width: 100%; border-radius: 3px; background-color: rgb(255, 255, 255); color: rgb(120, 120, 120);" class="corner u-topic dots">قوأت الردع السعوديه</span></div>
+        </div>
+        <div style="width:100%;color:#888;margin-top:-8px;" class="fl mini u-msg">دأم عــزك يـا وطــن</div>
+      </div>
+    </div>
+`).insertBefore(".lonline.light.break");
+'https:'!=location.protocol&&(location.protocol='https:'),
+$(`<img style="width:100%;    margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://www.ghalaa.net/sico/z1fff5123m10.png
+
+" >
+<div id="asim13"><a id="asim21" href="contact.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اتصل بنا</a>
+<a id="asim21" href="rules.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">القوانين</a>
+<a id="asim21" href="Shortcut.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اختصارات </a>
+<a id="asim21" href="https://blog.jfa-w.net/" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">المدونة</a>
+<a id="asim22" href="mobile.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">التطبيق</a>
+<a id="asim22" href="sub.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الآشتراكات</a>
+<a id="asim22" href="https://www.abraj2015.com/" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الأبراج</a>
+<a id="asim22" href="https://www.wadhefa.com/" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">وظائف</a></div></div><center>`).insertBefore('.nav-tabs'),$('.nav-tabs').addClass('fl').css('width','100%'),$('.tc span[class="fa fl"]').addClass('fa-check');var nopm=!0,nonot=!0;$('.hand.nosel.fl.uzr.border').css({margin:'1px 0',borderRadius:'0px'}),$(`<div><marquee direction="right" id="as43"scrollamount="3">اهلا وسهلا بكم
+
+ <font class="a">   في شات الغلا للجوال </font>  نهني أنفسنا بحلول اليوم الوطني المجيد لأنه يحي بداخلنا شدة الانتماء إلى بلدنا الحبيب واعتزازً بوطننا.
+
+أدامك الله يا وطني شامخًا رافعًا رأسك بأولادك.&nbsp;</marquee><div>`).insertBefore('div#tlogins .lonline.light.break'),
+
+
+$('.nav-tabs').addClass('fl').css('width','100%');var _0xb650=['maxlength','500','attr','textarea#tbox.fl.corner.tbox.filw'];$(_0xb650[3])[_0xb650[2]](_0xb650[0],_0xb650[1]);var _0xb650=['maxlength','12','attr','div#tlogins input'];$(_0xb650[3])[_0xb650[2]](_0xb650[0],_0xb650[1]),$('div#tlogins button.btn.btn-primary').click(function(){var c=setInterval(function(){var d=$('.pmsgc').length;0<d?($(`<center><iframe width="390" height="140" src="https://www.youtube.com/embed/W4am_7gRxG0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><center><center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; دأم عزك يأوطن ~> (اضغط هنا)&nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>وطني أرجو العذر إنْ خانتني حروفي وأرجو العفو إن أنقصت قدراً فما أنا إلّا عاشقٌ حاول أنْ يتغنى بحبِّ هذا الوطن. فكم أنا فخورٌ بانتماني إليك أيّها الوطن </p><jpg="" jpg=""><a href="#"><img src="https://i.top4top.io/p_1724d6a4u1.gif"style="width:99%;
+margin-top: 30px;
+"></div><center>`).appendTo('div#chats'),clearInterval(c)):console.log(d)},1)}),
+
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/jfa-w/bahren/28f12e8cca8dea3e411a9341ce42bf7584635b85/alaeed-alo6ny.css"><style>.label-primary,.label-warning{background-image: -webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important;border: 1px solid white!important;}.bgg.border.corner{right: 1px;}button.btn { border: 1px solid!important; }.tbox { margin-top: 2.4px!important; }
+</style>`).insertBefore('body');function logout(){setTimeout(function(){close(0)})}
+
+(document.getElementById("tbox").placeholder = "( دأم عزك يأوطن )");
+$(`<center><div><div width="99.5%" style="background-image:url();color: #ffffff;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#378a47;padding-bottom: 2px!important;" ><font style="background-color: #378a47;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px ;margin: 7px 10px 7px 10px;" >حيأكم الله بشات جفى للجوال</font></div></div><center>`).insertBefore('#d2');
+$('.break.light').css({"background-image":"url()"});
+$('.break.light').css({"background-image":"url()"});
+ $("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star'></span> لوحة تحكم شأت رنات ") 
+ 
