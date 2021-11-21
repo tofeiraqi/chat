a3b070099362a84a146d8a204d6cda279816ffb0
tofeiraqi <44017333+tofeiraqi@users.noreply.github.com>
2021-11-21 21:04:11 +0300

    Create jzaan.js

---
+++ jzaan.js
@@ -0,0 +1,36 @@
+$(`<img src="https://jzaan.com/sico/z1fgf49crd10.gif" class="fr" style="width:100%;margin-top: 0px;">
+<a id="pi" href="" style="border-radius: 5px 0px 5px 0px;width:23%;margin:2px;" class="fl mini  fa  fa-
+ btn btn-primary">التطبيق</a>
+<a id="pi" href="" style="border-radius: 5px 0px 5px 0px;width:23%;margin:2px;" class="fl mini  fa   fa-
+ btn btn-default">الاشتراكات</a>
+<a id="pi" href="" style="border-radius: 5px 0px 5px 0px;width:27%;margin:2px;" class="fl mini  fa fa-
+btn btn-primary">قوانين الموقع</a>
+<a id="pi" href="" style="border-radius: 5px 0px 5px 0px;width:22%;margin:2px;" class="fl mini  fa fa-
+btn btn-default">اتصل بنا</a>
+ `).insertBefore('.nav-tabs');$("div#tlogins button.btn.btn-primary").click(function(){var myVar=setInterval(function(){var usmsgw=$(".pmsgc").length;if(usmsgw>0){$(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://chatjawwal.com/sico/z1e3i6isuu10.jpg);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="    >
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">شات جيزان</span>
+        </div>
+        <br>
+      <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl" bis_skin_checked="1">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl" bis_skin_checked="1"> مرحباً بك 👋 <h1 style="display: inline-block;color: red;">1234</h1>    نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات جيزان للجوال</div>
+      </div>
+    `).appendTo('div#d2')
+clearInterval(myVar);}else{console.log(usmsgw)}},2000);})
+$('.nav-tabs').addClass('fl').css('width','100%');$('a[href="https://jzaan.com/"]').removeAttr('href');$(`<link href="https://fonts.googleapis.com/css?family=Aref+Ruqaa|El+Messiri" rel="stylesheet">
+<link rel="stylesheet" href="https://chatpalestine.me/chatjawwalV91.css">
+<style>
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #316C7D;font-family: 'El Messiri',FontAwesome;border-radius: 3px;}
+
+</style>`).insertBefore('body');$('.hand.nosel.fl.uzr.border').css({'margin':'0px',borderRight:'3px solid #316C7D',borderLeft:'3px solid #316C7D',borderTop:'1px solid #e2e1e1',borderBottom:'1px solid #e2e1e1',borderRadius:'3px'});$(`<center><div><div width="75.5%" style="background-color: #b7c3c5; color: #BF4A47;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"  
+><font style="background-color: #fff;border-radius: 0px 25px 0px 25px;font-family: 'Cairo', sans-serif, FontAwesome;padding: 2px 10px 2px 10px; margin:0px 25px 0px 25px;" >- ¦¦ - شات نبـض جيــزان للجوال - ¦¦ -</font></div></div><center>`).insertBefore('#d2');(document.getElementById("tbox").placeholder="مـآ يلـفظ من قـول الا لـديه رقـيب عتيـد  ");
+
+$(` <center><div width="99%" id="asim7"><font style="background-color:#FFFFFF;color:#BF4A47;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > 
+ (  دردشة الحائط ممنوعه)   </font></div><center>`).insertBefore('div#d2bc');$("[data-target=#wall]").attr('title',' فضلاً وليس امراً عدم ألكلام في ألحائط')
+$('.fl.ustat').css('width','3px'),$('.fa.fa-user-plus').html(`تسجيل`),document.getElementById('u1').placeholder='اكتب اسمك من 7 احرف فقط',document.getElementById('pass1').placeholder='كلمة المرور',document.getElementById('u2').placeholder='فخأمة طلتك تنورنا',document.getElementById('pass2').placeholder='㋡ بـاسوردك ولاتنساه يـ جميل',document.getElementById('u3').placeholder='♥ أكتب اسمك ولازم تحفظه ';
+$(".fa-gear").text("الاعدادات").css("width","");
