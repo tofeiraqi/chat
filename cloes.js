d537b1d31e6e0476632ff2653f4409b6be53421e
tofeiraqi <44017333+tofeiraqi@users.noreply.github.com>
2025-03-18 21:11:12 +0300

    Update cloes.js

--- cloes.js
+++ cloes.js
@@ -1,6 +1,1 @@
-$('div#l1 button,div#l2 button,div#l3 button').attr('onclick', '');
-$('div#l1 button,div#l2 button,div#l3 button').click(function(){
-
-alert('الموقع مغلق : جـافـا هوست');
-
-});
+*/
